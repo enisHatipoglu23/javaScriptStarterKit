867105db486a559a44814bb2ffc5d4c4b556e0d3
Enis Hatipoğlu <enishatipoglu.2001y@gmail.com>
2021-10-19 00:05:42 +0300

    18 Oct's lesson

---
+++ src/app.js
@@ -0,0 +1,18 @@
+console.log("sa");
+
+let dolarDun = 9.20
+
+let dolarBugun = 9.30
+
+{
+    let dolarDun = 9.10
+}
+
+
+let konutKredileri = ["a", "b", "c"]
+
+for(let i = 0; i < konutKredileri.length; i++){
+    console.log("A: " , i)
+}
+console.log()
+
